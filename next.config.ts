f79a071cd5f59c1f1d3367f08c86f56b92209154
alaaaljumaa <67071036+alaaaljumaa@users.noreply.github.com>
2026-06-24 12:16:36 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",          // generates static HTML in /out
+  basePath: "/portfolio-alaa-jm",   // your repo name
+  images: {
+    unoptimized: true,       // required: GH Pages can't run Next's image optimizer
+  },
 };
 
 export default nextConfig;
